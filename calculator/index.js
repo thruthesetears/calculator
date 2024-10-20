c446809a3c1540790153195991c1d8fa14ac25a6
thruthesetears <moog.johnrei.c220736@gmail.com>
2024-10-20 22:08:33 +0800

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,20 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+function deleteLastCharacter() {
+    display.value = display.value.slice(0, -1);
+}
